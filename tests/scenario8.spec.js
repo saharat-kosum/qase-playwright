73fc7a48d9776ad28dda5c18f64cef748db9a889
phai MacOS <pekopon_@live.fr>
2025-11-14 00:49:19 +0700

    update

--- tests/scenario8.spec.js
+++ tests/scenario8.spec.js
@@ -34,4 +34,45 @@ test.describe('Scenario 8', () => {
     await expect(page2.getByRole('heading', { name: 'Hans Miller' })).toBeVisible();
     await expect(page2.getByRole('heading', { name: '0' })).toBeVisible();
   });
+
+  test('SC8-TC2 Withdraw failed - Amount is decimal', async ({ page }) => {
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).click();
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).fill('8.5');
+    await page2.getByRole('button', { name: 'Confirm' }).nth(1).click();
+
+    const message = await page2.locator('[cid="w1"]').evaluate(el => el.validationMessage);
+    expect(message).toBe('Please enter a valid value. The two nearest valid values are 8 and 9.');
+  });
+
+  test('SC8-TC3 Withdraw failed - Amount is less than 0', async ({ page }) => {
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).click();
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).fill('-1');
+    await page2.getByRole('button', { name: 'Confirm' }).nth(1).click();
+
+    await expect(page2.getByText('The amount must be greater')).toBeVisible();
+  });
+
+  test('SC8-TC4 Withdraw failed - Amount is not a number', async ({ page }) => {
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).click();
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).fill('e');
+    await page2.getByRole('button', { name: 'Confirm' }).nth(1).click();
+
+    const message = await page2.locator('[cid="w1"]').evaluate(el => el.validationMessage);
+    expect(message).toBe('Please enter a number.');
+  });
+
+  test('SC8-TC5 Withdraw failed - Amount is more than balance', async ({ page }) => {
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).click();
+    await page2.getByRole('spinbutton', { name: 'Please put your amount:' }).nth(1).fill('20');
+    await page2.getByRole('button', { name: 'Confirm' }).nth(1).click();
+
+    await expect(page2.getByText('Your balance is not enough to')).toBeVisible();
+  });
+
+  test('SC8-TC6 Withdraw failed - Amount is empty', async ({ page }) => {
+    await page2.getByRole('button', { name: 'Confirm' }).nth(1).click();
+
+    const message = await page2.locator('[cid="w1"]').evaluate(el => el.validationMessage);
+    expect(message).toBe('Please fill out this field.');
+  });
 });
