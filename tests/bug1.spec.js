6baa72829c1f6d0ff0883b034542ea1279d169b1
phai MacOS <pekopon_@live.fr>
2025-11-16 00:17:09 +0700

    update delete before run

--- tests/bug1.spec.js
+++ tests/bug1.spec.js
@@ -1,6 +1,27 @@
 import { test, expect } from "@playwright/test";
+import { getMongoClient } from "../utils/mongoClient.js";
 
 test.describe("Scenario 1", () => {
+  let client;
+
+  test.beforeAll(async () => {
+    client = getMongoClient();
+    if (!client.topology || !client.topology.isConnected()) {
+      await client.connect();
+    }
+
+    const DB_NAME = process.env.DB_NAME;
+    await client.db(DB_NAME).collection("users").deleteOne({ accountId: "6870021009" });
+    console.log("✔ Removed account 6870021009");
+  });
+
+  test.afterAll(async () => {
+    if (client) {
+      await client.close();
+      console.log("✔ MongoDB connection closed");
+    }
+  });
+
   test("SC1-TC16 Register Fail -Lastname เป็นการพิมพ์ช่องว่าง (spacebar)", async ({
     page,
   }) => {
--- tests/bug2.spec.js
+++ tests/bug2.spec.js
@@ -1,6 +1,27 @@
 import { test, expect } from "@playwright/test";
+import { getMongoClient } from "../utils/mongoClient.js";
 
 test.describe("Scenario 1", () => {
+  let client;
+
+  test.beforeAll(async () => {
+    client = getMongoClient();
+    if (!client.topology || !client.topology.isConnected()) {
+      await client.connect();
+    }
+
+    const DB_NAME = process.env.DB_NAME;
+    await client.db(DB_NAME).collection("users").deleteOne({ accountId: "6870021012" });
+    console.log("✔ Removed account 6870021012");
+  });
+
+  test.afterAll(async () => {
+    if (client) {
+      await client.close();
+      console.log("✔ MongoDB connection closed");
+    }
+  });
+
   test("SC1-TC15 Register Fail -Firstname เป็นการพิมพ์ช่องว่าง (spacebar)", async ({
     page,
   }) => {
